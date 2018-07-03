17471c3b7f69aace767f2ab295fc2e97261b0cc9
gooddetails <32199539+southernrude@users.noreply.github.com>
2018-07-03 21:06:11 +0800

    Update clear.R

--- clear.R
+++ clear.R
@@ -30,3 +30,15 @@ GoodsDetails_629 <- GoodsDetails_629[order(GoodsDetails_629$商品ID),]
 GoodsDetails_703 <- GoodsDetails_703[order(GoodsDetails_703$商品ID),]
 
 Soldnum <- GoodsDetails_703$近期售出数 - GoodsDetails_629$近期售出数
+Price <- GoodsDetails_703$价格
+Alcohol_Content <- as.numeric(str_replace_all(GoodsDetails_703$酒精度,"[\t\n%]",""))
+Protected_indicator_of_origin <- str_replace_all(GoodsDetails_703$级别,"[\t\n]","")
+Reserve <- str_replace_all(GoodsDetails_703$年份,"[\t\n]","")
+Variety <- str_replace_all(GoodsDetails_703$葡萄品种,"[\t\n]","")
+COO <- str_replace_all(GoodsDetails_703$COO原产地,"[\t\n]","")
+Body <- str_replace_all(GoodsDetails_703$酒体,"[\t\n]","")
+Rating <- as.numeric(str_replace_all(GoodsDetails_703$评分,"[\t\n%]",""))
+No_of_reviews <- as.numeric(str_replace_all(GoodsDetails_703$评价数,"[\t\n]",""))
+
+Wine_Sample <- as.data.frame(cbind(Soldnum,Price,Alcohol_Content,Reserve,COO,Variety,Body,Protected_indicator_of_origin,Rating,No_of_reviews)) 
+write.csv(Wine_Sample,file = "F:\\wineSample.csv")
